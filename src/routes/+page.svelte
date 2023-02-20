2349b73a03c0434d6b7f00e6c45f7eedd711f080
aldwiputra <aldiansyhdp@gmail.com>
2023-02-20 10:31:16 +0700

    add tailwind config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Logo from '../lib/Logo.svelte';
+</script>
+
+<div class="flex flex-col items-center p-8">
+	<Logo />
+	<h1 class="text-3xl font-bold text-gray-400">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a class="text-purple-700 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read
+		the documentation
+	</p>
+</div>
+
+<style lang="postcss">
+	:global(body) {
+		@apply bg-gray-800;
+	}
+</style>
