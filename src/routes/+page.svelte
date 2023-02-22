64bf89634ff5cddeff19b296c18a687ebe46ae51
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-02-22 21:41:31 +0700

    adjust some stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
@@ -10,9 +9,9 @@
 </svelte:head>
 
 <main class="mx-auto max-w-screen-2xl p-0 md:mt-4">
-	<div class="grid grid-cols-1 grid-rows-1 overflow-hidden md:rounded-lg">
+	<div class="grid max-h-[30rem] grid-cols-1 grid-rows-1 overflow-hidden md:rounded-lg">
 		<img
-			class="saturate-20 col-span-full row-span-full h-full object-cover object-top opacity-[.15] blur-md md:rounded-md"
+			class="col-span-full row-span-full h-full object-cover object-top opacity-[.20] blur-md saturate-50 md:w-full md:rounded-md"
 			src={`https://image.tmdb.org/t/p/w1280/${data.mainMovie.backdrop_path}`}
 			alt="" />
 		<article
@@ -24,9 +23,9 @@
 
 			<div class=" flex flex-col justify-between self-stretch">
 				<span
-					class="border-1 w-fit rounded-full border border-solid border-sky-500 bg-sky-500/10 px-3 py-1 text-[10px] font-medium leading-tight text-sky-500">
+					class="border-1 flex w-fit items-center gap-1 rounded-full border border-solid border-sky-500 bg-sky-500/10 px-3 py-1 align-middle text-[10px] font-medium leading-snug text-sky-500">
 					<svg
-						class="mr-1 inline-block w-1.5"
+						class="mr-1 w-1.5 align-middle"
 						viewBox="0 0 24 26"
 						fill="none"
 						xmlns="http://www.w3.org/2000/svg">
@@ -34,16 +33,17 @@
 							class="fill-sky-500"
 							d="M23.04 11.4062C24.32 12.1145 24.32 13.8855 23.04 14.5938L2.88 25.7507C1.6 26.4591 0 25.5736 0 24.1568V1.84315C0 0.426403 1.6 -0.459058 2.88 0.249313L23.04 11.4062Z" />
 					</svg>
-					Now Playing</span>
+					<span> In theaters </span>
+				</span>
 				<h2
-					class="mt-2 max-w-[15ch] overflow-hidden text-ellipsis whitespace-nowrap text-xl font-semibold text-zinc-200">
+					class="text-md mt-2 max-w-[15ch] overflow-hidden text-ellipsis whitespace-nowrap font-extrabold text-zinc-200">
 					{data.mainMovie.title}
 				</h2>
-				<p class="mt-2 text-xs font-semibold text-zinc-200">
-					{`${data.mainMovie.release_date.split('-')[0]}   |   ${data.mainMovie.vote_average}`}
+				<p class="mt-2 text-xs font-bold text-zinc-200">
+					{`${data.mainMovie.release_date.split('-')[0]} | ${data.mainMovie.vote_average}`}
 				</p>
 
-				<p class="mt-2 max-w-[20ch] text-sm leading-normal text-zinc-400 line-clamp-4">
+				<p class="mt-2 max-w-[20ch] text-xs leading-normal text-gray-400  line-clamp-4">
 					{data.mainMovie.overview}
 				</p>
 			</div>
