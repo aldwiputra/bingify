d80747543dbcf393baf45170c73b1b2561fc7d89
aldwiputra <aldiansyhdp@gmail.com>
2023-02-22 15:24:27 +0700

    adjust some size and delete the unused

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,6 @@
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
-
-	let backgroundImgElement: HTMLElement;
-
-	onMount(() => {
-		backgroundImgElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w1280/${data.mainMovie.backdrop_path})`;
-	});
 </script>
 
 <svelte:head>
@@ -18,13 +12,13 @@
 <main class="mx-auto max-w-screen-2xl p-0 md:mt-4">
 	<div class="grid grid-cols-1 grid-rows-1 overflow-hidden md:rounded-lg">
 		<img
-			class="saturate-20 col-span-full row-span-full h-full object-cover object-top opacity-20 blur-md md:rounded-md"
+			class="saturate-20 col-span-full row-span-full h-full object-cover object-top opacity-[.15] blur-md md:rounded-md"
 			src={`https://image.tmdb.org/t/p/w1280/${data.mainMovie.backdrop_path}`}
 			alt="" />
 		<article
-			class="z-10 col-span-full row-span-full my-10 flex h-fit w-fit items-stretch gap-6 place-self-center">
+			class="z-10 col-span-full row-span-full my-10 flex h-fit w-fit items-stretch gap-4 place-self-center">
 			<img
-				class="w-24 rounded-md"
+				class="w-28 rounded-md"
 				src={`https://image.tmdb.org/t/p/w185/${data.mainMovie.poster_path}`}
 				alt="" />
 
@@ -33,11 +27,11 @@
 					class="max-w-[15ch] overflow-hidden text-ellipsis whitespace-nowrap text-xl font-semibold text-zinc-200">
 					{data.mainMovie.title}
 				</h2>
-				<p class="text-sm font-semibold text-zinc-200">
+				<p class="text-xs font-semibold text-zinc-200">
 					{`${data.mainMovie.release_date.split('-')[0]}   |   ${data.mainMovie.vote_average}`}
 				</p>
 
-				<p class="max-w-[20ch] text-sm leading-normal text-zinc-400 line-clamp-4">
+				<p class="max-w-[20ch] text-sm leading-normal text-zinc-400 line-clamp-5">
 					{data.mainMovie.overview}
 				</p>
 			</div>
